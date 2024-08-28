c0f12072fde93fcc98dea7b3f64d22b348015bdc
Denys Podobrii <denys.podobrii@gmail.com>
2024-08-28 10:52:17 +0300

    checkout scm func added

---
+++ vars/checkoutCode.groovy
@@ -0,0 +1,7 @@
+def call(String repoUrl, String branch) {
+    def workingDir = "${env.WORKSPACE}"
+    sh "git clone ${repoUrl} ${workingDir}"
+    sh "git switch ${branch}"
+    return workingDir
+}
+

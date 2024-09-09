8190bc2e7a266498d503df04765097232f593f85
Denys Podobrii <denys.podobrii@gmail.com>
2024-09-09 17:08:42 +0300

    use variable as name of docker file

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,9 +1,9 @@
 #! /usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     withCredentials([usernamePassword(credentialsId: 'docker-hub', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh 'docker build -t dennydobry/test-java-app:jma-1.3 .'
+        sh "docker build -t $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push dennydobry/test-java-app:jma-1.3'
+        sh "docker push $imageName"
     }
 }
